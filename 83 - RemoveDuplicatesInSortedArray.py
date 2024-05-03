5bac69282ee12673ccac55b662f4722d3a2325bf
Seaghan Ennis <seaghan00@gmail.com>
2024-05-02 19:42:48 -0600

    83

---
+++ 83 - RemoveDuplicatesInSortedArray.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def deleteDuplicates(self, head):
+        temp = head
+        while (temp and temp.next):
+            if (temp.next.val == temp.val):
+                temp.next = temp.next.next
+                continue
+            temp = temp.next
+        return head
